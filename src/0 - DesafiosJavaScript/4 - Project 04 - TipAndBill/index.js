6e918457151dee3f4eb19ccb850bf418539aa72f
rafael_torres_nantes <rafatorresnantes@gmail.com>
2024-03-30 18:53:24 -0400

    build : project 05, 06

---
+++ src/0 - DesafiosJavaScript/4 - Project 04 - TipAndBill/index.js
@@ -0,0 +1,21 @@
+function update() {
+    let bill = Number(document.getElementById('yourBill').value)
+    let tipPercent = document.getElementById('tipInput').value
+    let split = document.getElementById('splitInput').value
+
+    let tipValue = bill * (tipPercent / 100)
+    let billTotal = bill + tipValue
+    let billEach = billTotal / split
+
+    document.getElementById('tipPercent').innerHTML = tipPercent + ' %'
+    document.getElementById('tipValue').innerHTML = formatMoney(tipValue)
+    document.getElementById('totalWithTip').innerHTML = formatMoney(billTotal)
+    document.getElementById('splitValue').innerHTML = (split == 1) ? split + ' person' : split + ' people'
+    document.getElementById('billEach').innerHTML = formatMoney(billTotal / split)
+}
+
+function formatMoney(value) {
+    value = Math.ceil(value * 100) / 100
+    value = value.toFixed(2)
+    return '$ ' + value 
+} 
